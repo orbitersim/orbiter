4765c53583f9b427928dd8dd28dc363c9b48fea3
schnepe2 <schnepe2@gmx.de>
2012-11-16 19:58:33 +0000

    - Documentation source format changed to html
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@36 c0f5cd98-1510-604a-8459-bf7538b8c9c2

---
+++ Orbitersdk/D3D9Client/doc/createPDF.bat
@@ -0,0 +1,7 @@
+@rem This line assumes the Pyhton is installed system-wide
+python unoconv -f pdf -o ..\..\..\Doc\D3D9Client.pdf D3D9Client.html
+
+@rem If Phython is not installed system-wide, we can use LibreOffice's Phython
+@rem like this e.g.
+@rem C:\Program Files (x86)\LibreOffice 3.6\program\python.exe unoconv -f pdf -o D3D9Client.pdf D3D9Client.html
+
