4b78db3d021244f92f931a8a5eb9dda83b249a26
schnepe2 <schnepe2@gmx.de>
2013-05-22 20:50:35 +0000

    Documentation updates - updated doxygen and API-Reference file for (upcoming) R12 release. - minor typo fixed. - optimized D3D9 Debug Controls.png - re generated PDF.
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@174 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/D3D9Config.h
+++ Orbitersdk/D3D9Client/D3D9Config.h
@@ -58,14 +58,14 @@ public:
 	int EnvMapSize;					///< Environment map size (64...512)
 	int EnvMapMode;					///< Environment map mode (0=disabled, 1=planet only, 2=fullscene)
 	int EnvMapFaces;				///< Number of environment map faces render per frame (1..6, default=1)
-	int EnableGlass;				///< Enable improved glass shading (Fresnel rerflection)
+	int EnableGlass;				///< Enable improved glass shading (Fresnel reflection)
 	int EnableMeshDbg;				///< Enable mesh debugger
 	int ShadowMapMode;				///< Shadow Mapping Mode
 	int ShadowMapSize;				///< Shadow Map size
 	char *DebugFont;				///< Font face for debug lines (default="Fixed")
 	char *Shaders;					///< Shaders set to use. Naming a sub-directory of "Modules/" (default="Default")
 	char *SolCfg;					///< Solar system to use (default="Sol")
-	
+
 private:
 
 };
