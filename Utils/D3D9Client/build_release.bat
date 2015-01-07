568109258fa57daa7613be9b787b9dbec6227a78
schnepe2 <schnepe2@gmx.de>
2015-01-07 23:32:48 +0000

    - added 'build_release.bat' to Utils.
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@353 c0f5cd98-1510-604a-8459-bf7538b8c9c2

---
+++ Utils/D3D9Client/build_release.bat
@@ -0,0 +1,90 @@
+@echo off
+:: ----------------------------------------------------------------------------
+:: Batch file to create a build package (ZIP) of the D3D9Client
+::
+:: Notes:
+:: - you need to have 7-Zip installed (you might however still need to adjust
+::   the ZIP_CMD setting below to make it work)
+:: - The generated ZIP file name contains the revision number in brackets.
+::   If that number ends with an 'M' (like e.g. r35884M), it means that it
+::   was build containing local modifications!
+::   This should *not* be a officially released version!
+::
+:: ----------------------------------------------------------------------------
+
+:: --- Setup
+set BASE_DIR=..\..
+set OUT_DIR=_release
+set VERSION=Beta9-forRev10
+
+set ZIP_NAME=D3D9Client%VERSION%
+set SETVCVARS="%VS100COMNTOOLS%vsvars32.bat"
+set VC=msbuild.exe
+set BUILD_FLAG=/t:build
+set SOLUTIONFILE="%BASE_DIR%\Orbitersdk\D3D9Client\D3D9ClientVS2010.sln"
+set CONFIG=/p:Configuration=Release
+set ZIP_CMD="C:\Program Files\7-Zip\7z.exe"
+
+
+:: --- Update working copy & get revision number
+svn update --quiet %BASE_DIR%
+for /F "tokens=*" %%i IN ('svnversion %BASE_DIR%') DO set REVISION=%%i
+
+
+:: --- Create folder structure
+if exist "%OUT_DIR%" (
+  rmdir /S /Q "%OUT_DIR%"
+)
+mkdir "%OUT_DIR%"
+
+
+:: --- Build
+call %SETVCVARS% x86
+call %VC% %BUILD_FLAG% %SOLUTIONFILE% %CONFIG%
+
+
+:: --- Export
+set ABS_PATH=%cd%
+svn export --force --quiet "%BASE_DIR%\Config" "%ABS_PATH%\%OUT_DIR%\Config"
+svn export --force --quiet "%BASE_DIR%\Doc" "%ABS_PATH%\%OUT_DIR%\Doc"
+svn export --force --quiet "%BASE_DIR%\Meshes" "%ABS_PATH%\%OUT_DIR%\Meshes"
+svn export --force --quiet "%BASE_DIR%\Modules" "%ABS_PATH%\%OUT_DIR%\Modules"
+svn export --force --quiet "%BASE_DIR%\Orbitersdk" "%ABS_PATH%\%OUT_DIR%\Orbitersdk"
+svn export --force --quiet "%BASE_DIR%\Textures" "%ABS_PATH%\%OUT_DIR%\Textures"
+svn export --force --quiet "%BASE_DIR%\Utils" "%ABS_PATH%\%OUT_DIR%\Utils"
+svn export --force --quiet "%BASE_DIR%\D3D9Client.cfg" "%ABS_PATH%\%OUT_DIR%"
+
+
+:: --- Copy the DLL
+if not exist "%OUT_DIR%\Modules\Plugin" mkdir "%OUT_DIR%\Modules\Plugin"
+copy /y %BASE_DIR%\Modules\Plugin\D3D9Client.dll ^
+         %OUT_DIR%\Modules\Plugin\D3D9Client.dll > nul
+
+
+:: --- Packing
+cd %OUT_DIR%
+%ZIP_CMD% a -tzip -mx9 "..\%ZIP_NAME%(r%REVISION%)+src.zip" *
+
+rmdir /S /Q "Orbitersdk"
+rmdir /S /Q "Utils"
+%ZIP_CMD% a -tzip -mx9 "..\%ZIP_NAME%(r%REVISION%).zip" *
+
+
+:: --- Cleanup & exit
+cd %ABS_PATH%
+rmdir /S /Q "%OUT_DIR%"
+set BASE_DIR=
+set OUT_DIR=
+set VERSION=
+set REVISION=
+set SETVCVARS=
+set VC=
+set BUILD_FLAG=
+set SOLUTIONFILE=
+set CONFIG=
+set ABS_PATH=
+set ZIP_CMD=
+set ZIP_NAME=
+set SETVCVARS=
+
+exit /b 0
