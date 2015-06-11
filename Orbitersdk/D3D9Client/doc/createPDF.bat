72c3b94de8a50a83d52be5099f636bae1da9034b
schnepe2 <schnepe2@gmx.de>
2015-06-11 22:04:46 +0000

    Documentation layout changes - page numbers added. - margins (left/right) unified. - some CSS redesign.
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@394 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/doc/createPDF.bat
+++ Orbitersdk/D3D9Client/doc/createPDF.bat
@@ -1,40 +1,45 @@
-@rem ---------------------------------------------------------------------------
-@rem -- setup
-@rem ---------------------------------------------------------------------------
+@echo off
+:: ----------------------------------------------------------------------------
+:: -- setup
+:: ----------------------------------------------------------------------------
 
-@set WKHTMLTOPDF_PATH=C:\Program Files\wkhtmltopdf\bin
-@rem set PRG="C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
-@set INPUT=D3D9Client.html
-@set OUTPUT=..\..\..\Doc\D3D9Client.pdf
+set WKHTMLTOPDF_PATH=C:\Program Files\wkhtmltopdf\bin
+:: set PRG="C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"
+set INPUT=D3D9Client.html
+set OUTPUT=..\..\..\Doc\D3D9Client.pdf
 
-@rem ---------------------------------------------------------------------------
-@rem -- check
-@rem ---------------------------------------------------------------------------
+:: ----------------------------------------------------------------------------
+:: -- check
+:: ----------------------------------------------------------------------------
 
-@set PRG="%WKHTMLTOPDF_PATH%\wkhtmltopdf.exe"
-@if not exist %PRG% (
-  @echo Sorry, could not find wkhtmltopdf at '%WKHTMLTOPDF_PATH%'!
-  @echo   To create the PDF from the %INPUT% source wkhtmltopdf has to be
-  @echo   installed ^(http://wkhtmltopdf.org^)!
-  @echo.
-  @echo.
-  @pause
-  @goto :eof
+set PRG="%WKHTMLTOPDF_PATH%\wkhtmltopdf.exe"
+if not exist %PRG% (
+  echo Sorry, could not find wkhtmltopdf at '%WKHTMLTOPDF_PATH%'!
+  echo   To create the PDF from the %INPUT% source wkhtmltopdf has to be
+  echo   installed ^(http://wkhtmltopdf.org^)!
+  echo.
+  echo.
+  pause
+  goto :eof
 )
 
 
-@rem ---------------------------------------------------------------------------
-@rem -- run
-@rem ---------------------------------------------------------------------------
+:: ----------------------------------------------------------------------------
+:: -- run
+:: ----------------------------------------------------------------------------
 
-@%PRG% --margin-left 25mm --zoom 1.3 %INPUT% %OUTPUT%
+%PRG% --margin-left 20mm ^
+      --margin-right 20mm ^
+      --zoom 1.3 ^
+      --footer-center "- [page] -" ^
+      %INPUT% %OUTPUT%
 
 
-@rem ---------------------------------------------------------------------------
-@rem -- cleanup
-@rem ---------------------------------------------------------------------------
+:: ----------------------------------------------------------------------------
+:: -- cleanup
+:: ----------------------------------------------------------------------------
 
-@set WKHTMLTOPDF_PATH=
-@set PRG=
-@set INPUT=
-@set OUTPUT=
+set WKHTMLTOPDF_PATH=
+set PRG=
+set INPUT=
+set OUTPUT=
