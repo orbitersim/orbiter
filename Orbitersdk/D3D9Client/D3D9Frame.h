c64c1aa577d00564f3a61849a85eb88403d48835
schnepe2 <schnepe2@gmx.de>
2015-07-01 16:23:45 +0000

    - Fixed comparison of a boolean with an integer @ 'CD3DFramework9::GetVSync'   (FALSE vs. false). - Member variable 'CD3DFramework9::cfg' was not initialized in the constructor   => removed via comment.
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@407 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/D3D9Frame.h
+++ Orbitersdk/D3D9Client/D3D9Frame.h
@@ -2,10 +2,10 @@
 // Part of the ORBITER VISUALISATION PROJECT (OVP)
 // Dual licensed under GPL v3 and LGPL v3
 // Copyright (C) 2007 Martin Schweiger
-//				 2012 Jarmo Nikkanen
+//               2012 Jarmo Nikkanen
 // ==============================================================
 
-// ====================================================================================
+// ============================================================================
 // File: D3D9frame.h
 // Desc: Class to manage the Direct3D environment objects
 //
@@ -20,12 +20,12 @@
 //       restored with the RestoreSurfaces() function. Finally, if normal
 //       Windows output is needed, the FlipToGDISurface() provides a GDI
 //       surface to draw on.
-// ====================================================================================
+// ============================================================================
 
 #ifndef D3D9FRAME_H
 #define D3D9FRAME_H
 
-#include <d3d9.h> 
+#include <d3d9.h>
 #include <d3dx9.h>
 #include "orbitersdk.h"
 #include "D3D9Client.h"
@@ -40,77 +40,79 @@ class D3D9ClientSurface;
 class CD3DFramework9
 {
 
+private:
+
     // Internal variables for the framework class
     HWND                 hWnd;               // The window object
     BOOL                 bIsFullscreen;      // Fullscreen vs. windowed
-	BOOL				 bVertexTexture;
-	BOOL				 bAAEnabled;
-	BOOL				 bNoVSync;           // don't use vertical sync in fullscreen
-	BOOL				 Alpha;
-	BOOL				 SWVert;
-	BOOL				 Pure;
-	BOOL				 DDM;
-	BOOL				 bGDIBB;
-	BOOL				 nvPerfHud;
+    BOOL                 bVertexTexture;
+    BOOL                 bAAEnabled;
+    BOOL                 bNoVSync;           // don't use vertical sync in fullscreen
+    BOOL                 Alpha;
+    BOOL                 SWVert;
+    BOOL                 Pure;
+    BOOL                 DDM;
+    BOOL                 bGDIBB;
+    BOOL                 nvPerfHud;
     DWORD                dwRenderWidth;      // Dimensions of the render target
     DWORD                dwRenderHeight;
-	DWORD				 dwFSMode;
+    DWORD                dwFSMode;
     LPDIRECT3D9          pD3D;               // The Direct3D object
     LPDIRECT3DDEVICE9    pd3dDevice;         // The D3D device
-	LPD3DXFONT			 pLargeFont;
-	LPD3DXFONT			 pSmallFont;
-	DWORD				 dwZBufferBitDepth;  // Bit depth of z-buffer
-	DWORD                dwStencilBitDepth;  // Bit depth of stencil buffer (0 if none)
-	DWORD				 Adapter;
-	DWORD				 Mode;
-	DWORD				 MultiSample;
-	LPDIRECT3DSURFACE9	 pRenderTarget;
-	LPDIRECT3DSURFACE9	 pEnvDS;
-	LPDIRECT3DSURFACE9   pShmDS;
-	LPDIRECT3DTEXTURE9   pShmRT;
-	D3D9ClientSurface *  pBackBuffer;
-	class D3D9Config  *  cfg;
-	
-	RECT					rcScreenRect;       // Screen rect for window
-	
+    LPD3DXFONT           pLargeFont;
+    LPD3DXFONT           pSmallFont;
+    DWORD                dwZBufferBitDepth;  // Bit depth of z-buffer
+    DWORD                dwStencilBitDepth;  // Bit depth of stencil buffer (0 if none)
+    DWORD                Adapter;
+    DWORD                Mode;
+    DWORD                MultiSample;
+    LPDIRECT3DSURFACE9   pRenderTarget;
+    LPDIRECT3DSURFACE9   pEnvDS;
+    LPDIRECT3DSURFACE9   pShmDS;
+    LPDIRECT3DTEXTURE9   pShmRT;
+    D3D9ClientSurface *  pBackBuffer;
+//  class D3D9Config  *  cfg;
+
+    RECT                 rcScreenRect;       // Screen rect for window
+
     // Internal functions for the framework class
-  
+
     HRESULT CreateFullscreenMode();
     HRESULT CreateWindowedMode();
-	void	Clear();
-   
+    void    Clear();
+
 public:
 
-	D3DPRESENT_PARAMETERS	d3dPP;
-	D3DCAPS9				caps;
+    D3DPRESENT_PARAMETERS   d3dPP;
+    D3DCAPS9                caps;
 
     // Access functions for DirectX objects
-	inline HWND					GetRenderWindow() const		 { return hWnd; }
-    inline LPDIRECT3D9          GetDirect3D() const          { return pD3D; }
-    inline LPDIRECT3DDEVICE9    GetD3DDevice() const         { return pd3dDevice; }
-    inline DWORD                GetRenderWidth() const       { return dwRenderWidth; }      // Dimensions of the render target
-    inline DWORD                GetRenderHeight() const	     { return dwRenderHeight; }      // Dimensions of the render target
-	inline DWORD                GetZBufferBitDepth() const   { return dwZBufferBitDepth; }
-	inline DWORD                GetStencilBitDepth() const   { return dwStencilBitDepth; }
-	inline DWORD				GetWidth() const			 { return dwRenderWidth; }
-	inline DWORD				GetHeight() const			 { return dwRenderHeight; }
-	inline const RECT           GetScreenRect() const        { return rcScreenRect; }
-	inline LPDIRECT3DSURFACE9   GetBackBuffer() const		 { return pRenderTarget; }
-	inline LPDIRECT3DSURFACE9   GetEnvDepthStencil() const	 { return pEnvDS; }
-	inline LPDIRECT3DSURFACE9   GetShadowMapDepthStencil() const { return pShmDS; }
-	inline LPDIRECT3DTEXTURE9   GetShadowMapRenderTarget() const { return pShmRT; }
-	inline SURFHANDLE			GetBackBufferHandle() const  { return pBackBuffer; }
-	inline LPD3DXFONT			GetLargeFont() const		 { return pLargeFont; }
-	inline LPD3DXFONT			GetSmallFont() const		 { return pSmallFont; }
-	inline BOOL					IsFullscreen() const         { return bIsFullscreen; }
-	inline BOOL					IsGDIBB() const				 { return bGDIBB; }
-	inline BOOL					IsAAEnabled() const			 { return bAAEnabled; }
-	inline const D3DCAPS9 *		GetCaps() const				 { return &caps; }
-	inline BOOL					HasVertexTextureSup() const  { return bVertexTexture; }
-	inline BOOL					GetVSync() const { return (bNoVSync==false); }
+    inline HWND                GetRenderWindow() const          { return hWnd; }
+    inline LPDIRECT3D9         GetDirect3D() const              { return pD3D; }
+    inline LPDIRECT3DDEVICE9   GetD3DDevice() const             { return pd3dDevice; }
+    inline DWORD               GetRenderWidth() const           { return dwRenderWidth; }      // Dimensions of the render target
+    inline DWORD               GetRenderHeight() const          { return dwRenderHeight; }     // Dimensions of the render target
+    inline DWORD               GetZBufferBitDepth() const       { return dwZBufferBitDepth; }
+    inline DWORD               GetStencilBitDepth() const       { return dwStencilBitDepth; }
+    inline DWORD               GetWidth() const                 { return dwRenderWidth; }
+    inline DWORD               GetHeight() const                { return dwRenderHeight; }
+    inline const RECT          GetScreenRect() const            { return rcScreenRect; }
+    inline LPDIRECT3DSURFACE9  GetBackBuffer() const            { return pRenderTarget; }
+    inline LPDIRECT3DSURFACE9  GetEnvDepthStencil() const       { return pEnvDS; }
+    inline LPDIRECT3DSURFACE9  GetShadowMapDepthStencil() const { return pShmDS; }
+    inline LPDIRECT3DTEXTURE9  GetShadowMapRenderTarget() const { return pShmRT; }
+    inline SURFHANDLE          GetBackBufferHandle() const      { return pBackBuffer; }
+    inline LPD3DXFONT          GetLargeFont() const             { return pLargeFont; }
+    inline LPD3DXFONT          GetSmallFont() const             { return pSmallFont; }
+    inline BOOL                IsFullscreen() const             { return bIsFullscreen; }
+    inline BOOL                IsGDIBB() const                  { return bGDIBB; }
+    inline BOOL                IsAAEnabled() const              { return bAAEnabled; }
+    inline const D3DCAPS9 *    GetCaps() const                  { return &caps; }
+    inline BOOL                HasVertexTextureSup() const      { return bVertexTexture; }
+    inline BOOL                GetVSync() const                 { return (bNoVSync==FALSE); }
 
     // Creates the Framework
-	HRESULT Initialize(HWND hWnd, struct oapi::GraphicsClient::VIDEODATA *vData);
+    HRESULT Initialize(HWND hWnd, struct oapi::GraphicsClient::VIDEODATA *vData);
 
     HRESULT DestroyObjects();
 
@@ -126,7 +128,7 @@ public:
 #define D3DFW_STEREO        0x00000002 // Use stereo-scopic viewing
 #define D3DFW_ZBUFFER       0x00000004 // Create and use a zbuffer
 #define D3DFW_NO_FPUSETUP   0x00000008 // Don't use default DDSCL_FPUSETUP flag
-#define D3DFW_NOVSYNC		0x00000010 // Don't use vertical sync in fullscreen
+#define D3DFW_NOVSYNC       0x00000010 // Don't use vertical sync in fullscreen
 #define D3DFW_PAGEFLIP      0x00000020 // Allow page flipping in fullscreen
 
 
