ae758f29923a75245f6b688a89b798604d8aaaf3
jarmonik <jnikkanen@kymp.net>
2016-04-03 09:20:02 +0000

    - Version increment, missing file added
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@576 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/D3D9Client.cpp
+++ Orbitersdk/D3D9Client/D3D9Client.cpp
@@ -2246,9 +2246,9 @@ void D3D9Client::SplashScreen()
 	if (m>12) m=0;
 
 #ifdef _DEBUG
-	char dataA[]={"D3D9Client Beta 22 Debug Build [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta 23 Debug Build [" __DATE__ "]"};
 #else
-	char dataA[]={"D3D9Client Beta 22 Build [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta 23 Build [" __DATE__ "]"};
 #endif
 
 	char dataB[128]; sprintf_s(dataB,128,"Build %s %u 20%u [%u]", months[m], d, y, oapiGetOrbiterVersion());
