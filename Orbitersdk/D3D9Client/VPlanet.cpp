303f84b6387acd15a6095556f8c2db6ed0fd18ca
jarmonik <jnikkanen@kymp.net>
2016-04-04 07:22:19 +0000

    - Fixed Orbiter restart micro texture problem
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@582 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/VPlanet.cpp
+++ Orbitersdk/D3D9Client/VPlanet.cpp
@@ -1304,7 +1304,7 @@ bool vPlanet::ParseMicroTextures()
 	MicroCfgsIterator it = MicroCfgs.find(GetName());
 	if (it != MicroCfgs.end()) {
 		MicroCfg = it->second;
-		MicroCfgs.erase(it);
+		//MicroCfgs.erase(it);	 Keep the table allocated for Orbiter restarts from launchpad
 		return true;
 	}
 	return false;
