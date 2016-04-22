d8c83b8316ed027bc6a5fc8a5ef0b8bb7f2619b1
schnepe2 <schnepe2@gmx.de>
2016-04-22 20:29:01 +0000

    - bumped version to Beta 23.2 (forgotten to commit in r622)
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@623 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Utils/D3D9Client/build_release.bat
+++ Utils/D3D9Client/build_release.bat
@@ -15,7 +15,7 @@
 :: --- Setup
 set BASE_DIR=..\..
 set OUT_DIR=_release
-set VERSION=Beta23.1
+set VERSION=Beta23.2
 
 :: Enhance Version by Orbiter Version
 for /F "tokens=* USEBACKQ" %%i in (`over /N ..\..\Orbitersdk\lib\orbiter.lib`) do set OVER=%%i
