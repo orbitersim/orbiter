3cf4ca24785d1f2cf8d00f414d6c70a8e6bd9007
schnepe2 <schnepe2@gmx.de>
2016-04-24 20:56:23 +0000

    - bumped version to Beta 23.4
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@635 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/D3D9Client.cpp
+++ Orbitersdk/D3D9Client/D3D9Client.cpp
@@ -2260,9 +2260,9 @@ void D3D9Client::SplashScreen()
 	if (m>12) m=0;
 
 #ifdef _DEBUG
-	char dataA[]={"D3D9Client Beta 23.3 Debug Build [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta 23.4 Debug Build [" __DATE__ "]"};
 #else
-	char dataA[]={"D3D9Client Beta 23.3 Build [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta 23.4 Build [" __DATE__ "]"};
 #endif
 
 	char dataB[128]; sprintf_s(dataB,128,"Build %s %u 20%u [%u]", months[m], d, y, oapiGetOrbiterVersion());
