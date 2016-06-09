d41392e31af774d01ed4feec43f5616c9646303c
schnepe2 <schnepe2@gmx.de>
2016-06-09 21:27:52 +0000

    - removed binaries (gcAPI.lib) - 'build_release.bat' handles extra build of gcAPI_dbg.lib
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@677 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Utils/D3D9Client/build_release.bat
+++ Utils/D3D9Client/build_release.bat
@@ -25,21 +25,25 @@ set VERSION=%VERSION%-for%OVER%
 if not "%VS140COMNTOOLS%"=="" (
   set SETVCVARS="%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
   set SOLUTIONFILE=D3D9ClientVS2015.sln
+  set GCAPI_PROJECTFILE=gcAPI\gcAPI.vcxproj
   goto :assign
 )
 if not "%VS110COMNTOOLS%"=="" (
   set SETVCVARS="%VS110COMNTOOLS%vsvars32.bat"
   set SOLUTIONFILE=D3D9ClientVS2012.sln
+  set GCAPI_PROJECTFILE=gcAPI\gcAPI.vcxproj
   goto :assign
 )
 if not "%VS100COMNTOOLS%"=="" (
   set SETVCVARS="%VS100COMNTOOLS%vsvars32.bat"
   set SOLUTIONFILE=D3D9ClientVS2010.sln
+  set GCAPI_PROJECTFILE=gcAPI\gcAPI.vcxproj
   goto :assign
 )
 if not "%VS90COMNTOOLS%"=="" (
   set SETVCVARS="%VS90COMNTOOLS%vsvars32.bat"
   set SOLUTIONFILE=D3D9ClientVS2008.sln
+  set GCAPI_PROJECTFILE=gcAPI\gcAPI.vcxproj
   goto :assign
 )
 
@@ -48,7 +52,9 @@ set ZIP_NAME=D3D9Client%VERSION%
 set VC=msbuild.exe
 set BUILD_FLAG=/t:build
 set SOLUTIONFILE="%BASE_DIR%\Orbitersdk\D3D9Client\%SOLUTIONFILE%"
+set GCAPI_PROJECTFILE="%BASE_DIR%\Orbitersdk\D3D9Client\%GCAPI_PROJECTFILE%"
 set CONFIG=/p:Configuration=Release
+set CONFIG_DBG=/p:Configuration=Debug
 set ZIP_CMD="C:\Program Files\7-Zip\7z.exe"
 
 
@@ -64,8 +70,13 @@ if exist "%OUT_DIR%" (
 mkdir "%OUT_DIR%"
 
 
-:: --- Build
+:: --- Start build environment
 call %SETVCVARS% x86 || goto exit_nok
+
+:: gcAPI_dbg.lib (DEBUG)
+call %VC% %GCAPI_PROJECTFILE% %BUILD_FLAG% %CONFIG_DBG% || goto exit_nok
+
+:: D3D9Client & gcAPI.lib (RELEASE)
 call %VC% %BUILD_FLAG% %SOLUTIONFILE% %CONFIG% || goto exit_nok
 
 
@@ -81,17 +92,22 @@ svn export --force --quiet "%BASE_DIR%\Utils" "%ABS_PATH%\%OUT_DIR%\Utils"
 svn export --force --quiet "%BASE_DIR%\D3D9Client.cfg" "%ABS_PATH%\%OUT_DIR%"
 
 
-:: --- Copy the DLL
+:: --- Copy the DLLs
 if not exist "%OUT_DIR%\Modules\Plugin" mkdir "%OUT_DIR%\Modules\Plugin"
+if not exist "%OUT_DIR%\Orbitersdk\lib" mkdir "%OUT_DIR%\Orbitersdk\lib"
 copy /y %BASE_DIR%\Modules\Plugin\D3D9Client.dll ^
          %OUT_DIR%\Modules\Plugin\D3D9Client.dll > nul
+copy /y %BASE_DIR%\Orbitersdk\lib\gcAPI.lib ^
+         %OUT_DIR%\Orbitersdk\lib\gcAPI.lib > nul
+copy /y %BASE_DIR%\Orbitersdk\lib\gcAPI_dbg.lib ^
+         %OUT_DIR%\Orbitersdk\lib\gcAPI_dbg.lib > nul
 
 
 :: --- Packing
 cd %OUT_DIR%
 %ZIP_CMD% a -tzip -mx9 "..\%ZIP_NAME%(r%REVISION%)+src.zip" *
 
-rmdir /S /Q "Orbitersdk"
+rmdir /S /Q "Orbitersdk\D3D9Client"
 rmdir /S /Q "Utils"
 %ZIP_CMD% a -tzip -mx9 "..\%ZIP_NAME%(r%REVISION%).zip" *
 
@@ -120,7 +136,9 @@ set SETVCVARS=
 set VC=
 set BUILD_FLAG=
 set SOLUTIONFILE=
+set GCAPI_PROJECTFILE=
 set CONFIG=
+set CONFIG_DBG=
 set ABS_PATH=
 set ZIP_CMD=
 set ZIP_NAME=
