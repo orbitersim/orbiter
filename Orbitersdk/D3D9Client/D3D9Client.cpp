aee387dbd1f810c605cd56a71eef6db449778999
jarmonik <jnikkanen@kymp.net>
2016-08-28 21:14:51 +0000

    - Documentation updates
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@778 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/D3D9Client.cpp
+++ Orbitersdk/D3D9Client/D3D9Client.cpp
@@ -2235,9 +2235,9 @@ void D3D9Client::SplashScreen()
 	if (m>12) m=0;
 
 #ifdef _DEBUG
-	char dataA[]={"D3D9Client 2016 Edition RC-1 (Debug Build) [" __DATE__ "]"};
+	char dataA[]={"D3D9Client 2016 Edition R1 (Debug Build) [" __DATE__ "]"};
 #else
-	char dataA[]={"D3D9Client 2016 Edition RC-1 [" __DATE__ "]"};
+	char dataA[]={"D3D9Client 2016 Edition R1 [" __DATE__ "]"};
 #endif
 
 	char dataB[128]; sprintf_s(dataB,128,"Build %s %u 20%u [%u]", months[m], d, y, oapiGetOrbiterVersion());
