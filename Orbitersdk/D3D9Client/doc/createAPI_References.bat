d119975c80b83eb4a7135ddc6ddb397dff3385a0
schnepe2 <schnepe2@gmx.de>
2018-08-08 22:35:04 +0000

    - updates to documentation creator batch files   to make them 'more clever' (no functional change)
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@968 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/doc/createAPI_References.bat
+++ Orbitersdk/D3D9Client/doc/createAPI_References.bat
@@ -1,11 +1,12 @@
 @echo off
+setlocal
 :: ----------------------------------------------------------------------------
 :: Batch file to (re-)create the D3D9Client_API_Reference.chm
 ::                           and gcAPI.chm files
 ::
 :: Notes:
 :: - you need to have doxygen (rsp.doxywizzard) installed (you might however
-::   still need to adjust the DOXYGEN search below to make it work)
+::   still need to adjust the doxygen search below to make it work)
 ::
 :: ----------------------------------------------------------------------------
 
@@ -15,18 +16,18 @@
 ::
 for %%X in (doxygen.exe) do (set FOUND=%%~$PATH:X)
 if defined FOUND (
-  set DOXYGEN=doxygen.exe
-  goto :run
+  set doxygen=doxygen.exe
+  goto :find_dot
 )
 :: or maybe here...
-if exist "c:\Program Files\doxygen\bin\doxygen.exe" (
-  set DOXYGEN="c:\Program Files\doxygen\bin\doxygen.exe"
-  goto :run
+for /f "delims=" %%i in ('dir /B "%ProgramFiles%"^|find /I "doxygen"') do (
+  set doxygen="%ProgramFiles%\%%%i"
+  goto :find_dot
 )
 :: last try here...
-if exist "c:\Program Files (x86)\doxygen\bin\doxygen.exe" (
-  set DOXYGEN="c:\Program Files (x86)\doxygen\bin\doxygen.exe"
-  goto :run
+for /f "delims=" %%i in ('dir /B "%ProgramFiles(x86)%"^|find /I "doxygen"') do (
+  set doxygen="%ProgramFiles(x86)%\%%%i"
+  goto :find_dot
 )
 
 :: --- failed to find doxygen ---
@@ -40,12 +41,45 @@ pause
 goto :eof
 
 
+::
+:: Try to find dot...
+::
+:find_dot
+for %%X in (dot.exe) do (set FOUND_DOT=%%~$PATH:X)
+if defined FOUND_DOT (
+  goto :run
+)
+:: or maybe here...
+for /f "delims=" %%i in ('dir /B "%ProgramFiles%"^|find /I "graphviz"') do (
+  set graphviz_path="%ProgramFiles%\%%%i"
+  goto :extend_path
+)
+:: last try here...
+for /f "delims=" %%i in ('dir /B "%ProgramFiles(x86)%"^|find /I "graphviz"') do (
+  set graphviz_path="%ProgramFiles(x86)%\%%%i"
+  goto :extend_path
+)
+
+:: --- failed to find dot ---
+echo.
+echo Sorry, could not find dot.exe anywhere!
+echo   To create the API from the source^(s^) dot.exe
+echo   ^(rsp. graphviz^) has to be installed ^(http://graphviz.org^)!
+echo.
+echo.
+pause
+goto :eof
+
+
+:: --- Extend PATH locally, so doxygen can use it ---
+:extend_path
+set graphviz_path=%graphviz_path:"=%
+::"
+set "PATH=%path%;%graphviz_path%"
+
+
 :: --- Do the jobs ---
 :run
 
-%DOXYGEN% Doxyfile
-%DOXYGEN% Doxyfile-gcAPI
-
-:: --- clean up ---
-set FOUND=
-set DOXYGEN=
+%doxygen% Doxyfile
+%doxygen% Doxyfile-gcAPI
