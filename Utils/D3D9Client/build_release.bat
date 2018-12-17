610af1e00159d191a2cf977c59126b5f1195fbf6
schnepe2 <schnepe2@gmx.de>
2018-12-17 22:14:11 +0000

    - bumped version to Beta 28.1
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@1026 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Utils/D3D9Client/build_release.bat
+++ Utils/D3D9Client/build_release.bat
@@ -16,7 +16,7 @@ setlocal
 :: --- Setup
 set BASE_DIR=..\..
 set OUT_DIR=_release
-set VERSION=Beta28.0
+set VERSION=Beta28.1
 
 :: Enhance Version by Orbiter Version
 for /F "usebackq tokens=*" %%i in (`over /N ..\..\Orbitersdk\lib\orbiter.lib`) do set OVER=%%i
