2e271f470dcdd3acb2791498974a942fb482eefa
jarmonik <jnikkanen@kymp.net>
2018-12-23 18:14:39 +0000

    - bumped version to Beta 28.3
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@1037 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/D3D9Client.cpp
+++ Orbitersdk/D3D9Client/D3D9Client.cpp
@@ -2474,9 +2474,9 @@ void D3D9Client::SplashScreen()
 	if (m>12) m=0;
 
 #ifdef _DEBUG
-	char dataA[]={"D3D9Client Beta R28.2 (Debug Build) [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta R28.3 (Debug Build) [" __DATE__ "]"};
 #else
-	char dataA[]={"D3D9Client Beta R28.2 [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta R28.3 [" __DATE__ "]"};
 #endif
 
 	char dataB[128]; sprintf_s(dataB,128,"Build %s %lu 20%lu [%u]", months[m], d, y, oapiGetOrbiterVersion());
