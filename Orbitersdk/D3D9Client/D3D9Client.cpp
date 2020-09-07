0f18cd2391f5e20c354e904c1a2d4634951bfbd4
jarmonik <jnikkanen@kymp.net>
2020-09-07 06:16:51 +0000

    - bumped version to Beta 30.1
    git-svn-id: svn://mirror.orbiter-radio.co.uk/D3D9client/trunk@1356 c0f5cd98-1510-604a-8459-bf7538b8c9c2

--- Orbitersdk/D3D9Client/D3D9Client.cpp
+++ Orbitersdk/D3D9Client/D3D9Client.cpp
@@ -2789,9 +2789,9 @@ void D3D9Client::SplashScreen()
 	if (m>12) m=0;
 
 #ifdef _DEBUG
-	char dataA[]={"D3D9Client Beta R30.0 (Debug Build) [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta R30.1 (Debug Build) [" __DATE__ "]"};
 #else
-	char dataA[]={"D3D9Client Beta R30.0 [" __DATE__ "]"};
+	char dataA[]={"D3D9Client Beta R30.1 [" __DATE__ "]"};
 #endif
 
 	char dataB[128]; sprintf_s(dataB,128,"Build %s %lu 20%lu [%u]", months[m], d, y, oapiGetOrbiterVersion());
