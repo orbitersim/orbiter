f629e5bb3ca3836545a0fd9039119098ff81393d
mschweiger <martins0815@googlemail.com>
2021-07-10 13:59:36 +0100

    Added FOLDER properites to all targets to group them in the VS IDE

--- Textures/mklinks.bat
+++ Textures/mklinks.bat
@@ -2,5 +2,5 @@ set tgtdir=%1
 
 for %%p in (Mercury Earth Moon Mars Phobos Deimos Vesta Jupiter Io Ganymede Callisto Europa Mimas Enceladus Tethys Dione Rhea Iapetus Titan Hyperion) do (
 	if exist %%p rmdir %%p
-	mklink /J %%p %tgtdir%\%%p
+	if not exist %%p mklink /J %%p %tgtdir%\%%p
 )
