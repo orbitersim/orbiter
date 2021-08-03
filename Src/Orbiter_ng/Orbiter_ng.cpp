e31ffb2251cd1c088bb6b56177e60e5e4bb853db
Martin Schweiger <martins0815@googlemail.com>
2021-08-03 04:33:16 +0100

    #37: use _execv in Orbiter_ng wrapper to pass command line arguments through to orbiter server. Now also returns the server exit value.

--- Src/Orbiter_ng/Orbiter_ng.cpp
+++ Src/Orbiter_ng/Orbiter_ng.cpp
@@ -6,8 +6,6 @@
 
 INT WINAPI WinMain (HINSTANCE hInstance, HINSTANCE, LPSTR strCmdLine, INT nCmdShow)
 {
-	const char *cmd = "modules\\server\\orbiter.exe";
-	char *args[1] = {NULL};
-	_execl(cmd, cmd, NULL);
-	return 0;
+	const char *cmd = "Modules\\Server\\Orbiter.exe";
+	return _execv(cmd, __argv);
 }
