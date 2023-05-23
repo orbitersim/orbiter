a9b4651e91b06a216e76b75bd7f7f8f3e30f0a29
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-05-23 14:17:02 -0400

    TreeFileHeader: remove class name

--- Src/Orbiter/ZTreeMgr.h
+++ Src/Orbiter/ZTreeMgr.h
@@ -35,8 +35,8 @@ class TreeFileHeader {
 
 public:
 	TreeFileHeader();
-	size_t TreeFileHeader::fwrite(FILE *f);
-	bool TreeFileHeader::fread(FILE *f);
+	size_t fwrite(FILE *f);
+	bool fread(FILE *f);
 
 private:
 	BYTE magic[4];      // file ID and version
