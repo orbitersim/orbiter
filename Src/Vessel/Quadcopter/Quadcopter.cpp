a6d5295868b46f80832d37c1b79c17115dfac7fe
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-06-20 16:49:46 -0400

    Quadcopter: clean-up
    Remove unnecessary _V().
    Use new vector functions.

--- Src/Vessel/Quadcopter/Quadcopter.cpp
+++ Src/Vessel/Quadcopter/Quadcopter.cpp
@@ -22,24 +22,24 @@ Quadcopter::Quadcopter(OBJHANDLE hObj, int fmodel)
 void Quadcopter::clbkSetClassCaps(FILEHANDLE cfg)
 {
 	const TOUCHDOWNVTX tdv[12] = {
-		{ _V(-0.3, -0.26,  0.3), 1e3, 5e1, 1.6, 1.6 },
-		{ _V(-0.3, -0.26, -0.3), 1e3, 5e1, 1.6, 1.6 },
-		{ _V( 0.3, -0.26, -0.3), 1e3, 5e1, 1.6, 1.6 },
-		{ _V( 0.3, -0.26,  0.3), 1e3, 5e1, 1.6 },
-		{ _V( 0.6, 0.15,  0.4), 1e3, 5e1, 1.6 },
-		{ _V( 0.6, 0.15, -0.4), 1e3, 5e1, 1.6 }, 
-		{ _V(-0.6, 0.15,  0.4), 1e3, 5e1, 1.6 },
-		{ _V(-0.6, 0.15, -0.4), 1e3, 5e1, 1.6 },
-		{ _V( 0.4, 0.15,  0.6), 1e3, 5e1, 1.6 },
-		{ _V(-0.4, 0.15,  0.6), 1e3, 5e1, 1.6 },
-		{ _V( 0.4, 0.15, -0.6), 1e3, 5e1, 1.6 },
-		{ _V(-0.4, 0.15, -0.6), 1e3, 5e1, 1.6 }
+		{ {-0.3, -0.26,  0.3}, 1e3, 5e1, 1.6, 1.6 },
+		{ {-0.3, -0.26, -0.3}, 1e3, 5e1, 1.6, 1.6 },
+		{ { 0.3, -0.26, -0.3}, 1e3, 5e1, 1.6, 1.6 },
+		{ { 0.3, -0.26,  0.3}, 1e3, 5e1, 1.6 },
+		{ { 0.6, 0.15,  0.4}, 1e3, 5e1, 1.6 },
+		{ { 0.6, 0.15, -0.4}, 1e3, 5e1, 1.6 }, 
+		{ {-0.6, 0.15,  0.4}, 1e3, 5e1, 1.6 },
+		{ {-0.6, 0.15, -0.4}, 1e3, 5e1, 1.6 },
+		{ { 0.4, 0.15,  0.6}, 1e3, 5e1, 1.6 },
+		{ {-0.4, 0.15,  0.6}, 1e3, 5e1, 1.6 },
+		{ { 0.4, 0.15, -0.6}, 1e3, 5e1, 1.6 },
+		{ {-0.4, 0.15, -0.6}, 1e3, 5e1, 1.6 }
 	};
 
 	SetEmptyMass(2.0);
 	SetSize(0.3);
-	SetPMI(_V(0.02, 0.05, 0.02));
-	SetCrossSections(_V(0.1, 0.6, 0.1));
+	SetPMI({0.02, 0.05, 0.02});
+	SetCrossSections({0.1, 0.6, 0.1});
 	SetCW(1.0, 1.0, 1.0, 1.0);
 	SetTouchdownPoints(tdv, 12);
 	exmesh_tpl = oapiLoadMeshGlobal("Quadcopter\\quadcopter");
@@ -48,7 +48,7 @@ void Quadcopter::clbkSetClassCaps(FILEHANDLE cfg)
 	// Dummy thruster
 	ph = CreatePropellantResource(0.01, 0.01);
 	SetDefaultPropellantResource(ph);
-	th = CreateThruster(_V(0, 0, 0), _V(0, 1, 0), 1e-10, ph, 1e-5, 1e-5);
+	th = CreateThruster({0, 0, 0}, {0, 1, 0}, 1e-10, ph, 1e-5, 1e-5);
 	tgh = CreateThrusterGroup(&th, 1, THGROUP_MAIN);
 }
 
