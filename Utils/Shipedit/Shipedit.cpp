3c14ddf137d5352a0328f77a85b113ed0801d175
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-07-09 22:28:09 -0400

    Shipedit: clean-up & use standard functions/operators

--- Utils/Shipedit/Shipedit.cpp
+++ Utils/Shipedit/Shipedit.cpp
@@ -317,20 +317,20 @@ void CShipeditApp::InitMesh ()
 		pp[i].d = -(pp[i].x1*(pp[i].y2*pp[i].z3-pp[i].y3*pp[i].z2) - pp[i].y1*(pp[i].x2*pp[i].z3-pp[i].x3*pp[i].z2) + pp[i].z1*(pp[i].x2*pp[i].y3-pp[i].x3*pp[i].y2));
 
 		Vector dr, a;
-		dr.Set (pp[i].x1-pp[i].x2, pp[i].y1-pp[i].y2, pp[i].z1-pp[i].z2);
-		a.Set  (pp[i].x3-pp[i].x2, pp[i].y3-pp[i].y2, pp[i].z3-pp[i].z2);
-		pp[i].d1 = (float)(dotp (dr, a)/a.length2());
-		dr.Set (pp[i].x2-pp[i].x3, pp[i].y2-pp[i].y3, pp[i].z2-pp[i].z3);
-		a.Set  (pp[i].x1-pp[i].x3, pp[i].y1-pp[i].y3, pp[i].z1-pp[i].z3);
-		pp[i].d2 = (float)(dotp (dr, a)/a.length2());
-		dr.Set (pp[i].x3-pp[i].x1, pp[i].y3-pp[i].y1, pp[i].z3-pp[i].z1);
-		a.Set  (pp[i].x2-pp[i].x1, pp[i].y2-pp[i].y1, pp[i].z2-pp[i].z1);
-		pp[i].d3 = (float)(dotp (dr, a)/a.length2());
+		dr = {pp[i].x1 - pp[i].x2, pp[i].y1 - pp[i].y2, pp[i].z1 - pp[i].z2};
+		a  = {pp[i].x3 - pp[i].x2, pp[i].y3 - pp[i].y2, pp[i].z3 - pp[i].z2};
+		pp[i].d1 = (float)(dot(dr, a) / len(a));
+		dr = {pp[i].x2 - pp[i].x3, pp[i].y2 - pp[i].y3, pp[i].z2 - pp[i].z3};
+		a  = {pp[i].x1 - pp[i].x3, pp[i].y1 - pp[i].y3, pp[i].z1 - pp[i].z3};
+		pp[i].d2 = (float)(dot(dr, a) / len(a));
+		dr = {pp[i].x3 - pp[i].x1, pp[i].y3 - pp[i].y1, pp[i].z3 - pp[i].z1};
+		a  = {pp[i].x2 - pp[i].x1, pp[i].y2 - pp[i].y1, pp[i].z2 - pp[i].z1};
+		pp[i].d3 = (float)(dot(dr, a) / len(a));
 	}
 
 	vol = 0.0;
-	cg.Set(0,0,0); cg_base.Set(0,0,0); cg_add.Set(0,0,0);
-	cs.Set(0,0,0);
+	cg = cg_base = cg_add = {0, 0, 0};
+	cs = {0, 0, 0};
 	J.Set(0,0,0,0,0,0,0,0,0); J_base.Set(J); J_add.Set(J);
 	nop = 0, nvol = 0;
 	for (i = 0; i < 3; i++) ncs[i] = 0;
@@ -370,7 +370,7 @@ void CShipeditApp::ProcessPackage ()
 	cg_add += cg_tmp;
 	J_add += J_tmp;
 	if (!(++flushcount % 1000)) {
-		cg_base += cg_add;  cg_add.Set(0,0,0);
+		cg_base += cg_add;  cg_add = {0, 0, 0};
 		J_base  += J_add;   J_add.Set (0,0,0,0,0,0,0,0,0);
 	}
 	vol = bbvol * (double)nvol / (double)nop;
