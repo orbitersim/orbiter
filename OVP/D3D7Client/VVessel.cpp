20c0aa56278d06e0b2225d45709a3bedf183e5a5
Matthew W Hume <mtthwhm@gmail.com>
2024-01-06 00:47:00 -0500

    testing

--- OVP/D3D7Client/VVessel.cpp
+++ OVP/D3D7Client/VVessel.cpp
@@ -174,7 +174,7 @@ void vVessel::UpdateRenderVectors()
 			if (logscale) len = log(len + 1e-5) - log(1e-5); else len *= scale * 1e5;
 			AddVector(unit(F) * (len * pscale), _V(0, 0, 0), scale2 * 0.5, std::string(cbuf), _V(1, 0, 1), alpha, D3DRGB(1, 0, 1));
 		}
-		if ((flag & BFV_TOTAL) && vessel->GetSideForceVector(F)) {
+		if ((flag & BVF_SIDEFORCE) && vessel->GetSideForceVector(F)) {
 			sprintf(cbuf, "SF = %fN", len = length(F));
 			if (logscale) len = log(len + shift) - lshift; else len *= scale;
 			AddVector(unit(F) * (len * pscale), _V(0, 0, 0), scale2, std::string(cbuf), _V(0, 0, 1), alpha, D3DRGB(0.5, 0.0, 1.0));
