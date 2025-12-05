6f61416a497b22912b056028c22ff7d21e15fb78
Gondos <thegondos@protonmail.com>
2025-12-05 03:35:03 +0100

    [ImGui]Update DlgFocus

--- Src/Orbiter/DlgFocus.h
+++ Src/Orbiter/DlgFocus.h
@@ -8,42 +8,18 @@
 #ifndef __DLGFOCUS_H
 #define __DLGFOCUS_H
 
-#include "DialogWin.h"
-#include "CommCtrl.h"
+#include "OrbiterAPI.h"
 
-class DlgFocus: public DialogWin {
+class DlgFocus : public ImGuiDialog {
 public:
-	DlgFocus (HINSTANCE hInstance, HWND hParent, void *context);
-	~DlgFocus ();
-	BOOL OnInitDialog (HWND hWnd, WPARAM wParam, LPARAM lParam);
-	BOOL OnSize (HWND hDlg, WPARAM wParam, int w, int h);
-	BOOL OnCommand (HWND hWnd, WORD id, WORD code, HWND hControl);
-	BOOL OnUserMessage (HWND hWnd, WPARAM wParam, LPARAM lParam);
-	BOOL OnNotify (HWND hDlg, int idCtrl, LPNMHDR pnmh);
-	BOOL OnHScroll (HWND hDlg, WORD request, WORD curpos, HWND hControl);
-
-protected:
-	BOOL OnNotifyTab (HWND hDlg, LPNMHDR pnmh);
-	BOOL OnNotifyTree (HWND hDlg, LPNMHDR pnmh);
-	void SwitchTab (HWND hDlg);
-	void RescanTree_All (HWND hDlg);
-	void RescanTree_Nearby (HWND hDlg);
-	void RescanTree_Location (HWND hDlg);
-	void RescanTree_Class (HWND hDlg);
-	HTREEITEM AddVesselToTree (HWND hDlg, HTREEITEM hti, Vessel *vessel, bool force = false);
-	BOOL SelectVessel (HWND hDlg);
-	void SelectPreviousVessel (HWND hDlg);
-	void UpdateFocus (HWND hDlg);
-	void WatchEdit (HWND hDlg);
-	void WatchAssemblyUnroll (HWND hDlg);
-	void SetRangeLabel (HWND hDlg, int irange);
-	HTREEITEM FindItem (HWND hDlg, const char *str, HTREEITEM hti_first);
-
-private:
-	int ctab;
-	int irange;
-	bool unroll_assemblies;
-	RECT rClient, rTab, rEdit, rTree, rAppl, rPrev, rHelp, rClse, rSldr, rLabl, rChck;
+    DlgFocus();
+    void OnDraw() override;
+    void DrawAll();
+    void DrawNearby();
+    void DrawLocation();
+    void DrawClass();
+    std::string m_SelectedShip;
+    float m_Range = 1.0;
 };
 
 #endif // !__DLGFOCUS_H
