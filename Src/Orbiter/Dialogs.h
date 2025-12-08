c58e9efa254ba518eaac6e6af745825a4bc02f55
Gondos <thegondos@protonmail.com>
2025-12-08 19:13:22 +0100

    [ImGui]Remove useless Dialogs.cpp

--- Src/Orbiter/Dialogs.h
+++ Src/Orbiter/Dialogs.h
@@ -14,15 +14,4 @@
 #include "DlgCapture.h"
 #include "DlgOptions.h"
 
-// =========================================================================
-// Set up a combo box dialog control for selecting a celestial body
-
-class CBodySelectComboBox {
-public:
-	CBodySelectComboBox () {}
-	static void Init (HWND hDlg, int resid);
-	static void BuildListFromNode (HWND hDlg, int resid, const CelestialBody *node = 0);
-	static CelestialBody *OnSelectionChanged (HWND hDlg, int resid);
-};
-
 #endif // !__DIALOGS_H
