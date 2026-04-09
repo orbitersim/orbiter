d58bc736c831f91d382179c522d719196b1a71c1
EvESpirit <40306186+EvESpirit@users.noreply.github.com>
2026-04-09 17:29:25 +0200

    Removed accicental inclusion of unverified SSL context

--- compileOrbiter.py
+++ compileOrbiter.py
@@ -11,13 +11,6 @@ import ssl
 import threading
 import concurrent.futures
 
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
 # ANSI escape
 if sys.platform == 'win32':
     os.system("")
