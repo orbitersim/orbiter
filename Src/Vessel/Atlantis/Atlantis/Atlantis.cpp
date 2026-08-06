012d95efe60854f3a40827d0d49b742aca9826c6
nadir-line <70193215+nadir-line@users.noreply.github.com>
2026-08-06 12:30:44 +0800

    Update Atlantis.cpp

--- Src/Vessel/Atlantis/Atlantis/Atlantis.cpp
+++ Src/Vessel/Atlantis/Atlantis/Atlantis.cpp
@@ -523,8 +523,8 @@ void Atlantis::VLiftCoeff (double aoa, double M, double Re, double *cl, double *
 	double cm_low = CMMachLow[idx] + (CMMachLow[idx+1]-CMMachLow[idx])*d;
 	double cl_high = CLMachHigh[idx] + (CLMachHigh[idx+1]-CLMachHigh[idx])*d;
 	double cm_high = CMMachHigh[idx] + (CMMachHigh[idx+1]-CMMachHigh[idx])*d;
-    double cd_prof_low = 0.055 + 0.002 * sin(aoa); // profile drag coefficient at low Mach
-    double cd_prof_high = 0.3 + 2.00 * pow(sin(aoa), 2); // profile drag coefficient at high Mach
+    double cd_prof_low = 0.055 + 0.002 * pow(sin(aoa), 2);  // profile drag coefficient at low Mach
+    double cd_prof_high = 0.5 + 2.00 * pow(sin(aoa), 2); // profile drag coefficient at high Mach
 
 	*cl = cl_low + (cl_high-cl_low)*mach_blend;
 	*cm = cm_low + (cm_high-cm_low)*mach_blend;
